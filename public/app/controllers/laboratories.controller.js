37ce938764d17f02af3b53b5371d969aca69beab
Henry Miskin <henry.miskin@talktalk.net>
2015-09-15 18:32:32 +0100

    Created laboratories section template

---
+++ public/app/controllers/laboratories.controller.js
@@ -0,0 +1,68 @@
+angular.module('turingWeb').controller('LaboratoriesController',['$scope',LaboratoriesController]);
+
+function LaboratoriesController($scope){
+
+    $scope.heading = 'Laboratories';
+
+    $scope.labs = [
+    {
+        name: 'Saturdays',
+        location: 'Imperial College London',
+        slots: [
+            {
+                name: 'Afternoon',
+                content: 'KS3 & Python',
+                time: '14.00 - 17.00'
+            },{
+                name: 'Morning',
+                content: 'KS2 & Scratch',
+                time: '10.00 - 13.00'
+            }
+        ],
+        prices: [
+            {
+                cost: '£225',
+                about: '10 Sessions',
+                hourly: '£7.50'
+            },{
+                cost: '£30',
+                about: '1 Session',
+                hourly: '£10.00'
+            }
+        ]
+    },{
+        name: 'Weekdays',
+        location: 'your local school',
+        slots: [
+            {
+                name: 'After-School',
+                content: 'KS3 & Python',
+                time: '2 hours'
+            }
+        ],
+        prices: [
+            {
+                cost: '£150',
+                about: '10 Sessions',
+                hourly: '£7.50'
+            },{
+                cost: '£20',
+                about: '1 Session',
+                hourly: '£10.00'
+            }
+        ]
+    },{
+        name: 'Workshop',
+        location: 'your local school',
+        slots: [
+            {
+                name: 'During-School',
+                content: 'KS2 & KS3',
+                time: '1-3 hours'
+            }
+        ],
+        about: 'Contact Us'
+    }
+    ];
+
+}
